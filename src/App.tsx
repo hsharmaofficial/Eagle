db5e7df512049726a3b77c02045b59b784e793e7
coscu23 <noreply@coscu.site>
2025-04-03 17:55:37 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import CookieConsent from "./components/CookieConsent";
 import Home from "./components/home";
